2264de3c0302978f29f855c5c69ae478b42155f2
Charlike Mike Reagent <opensource@tunnckocore.com>
2019-09-18 02:57:28 +0300

    use stringContaining instead
    Signed-off-by: Charlike Mike Reagent <opensource@tunnckocore.com>

--- integrationTests/passing.test.js
+++ integrationTests/passing.test.js
@@ -1,5 +1,6 @@
 const runJest = require('./runJest');
 
-it('Works when it has only passing tests', () => {
-  return expect(runJest('passing')).resolves.toMatchSnapshot();
+it('Works when it has skipped tests', async () => {
+  const resultString = await runJest('skipped');
+  expect(resultString).toEqual(expect.stringContaining('1 skipped, 1 total'));
 });
