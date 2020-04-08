a5e998c99b0ed6af059a786dde20263d49c2bf92
Simen Bekkhus <sbekkhus91@gmail.com>
2020-04-08 09:21:23 +0200

    chore: use `prepare` rather than `prepublish`

--- generator/fixtures/index.js
+++ generator/fixtures/index.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/no-unresolved
 const { createJestRunner } = require('create-jest-runner');
 
 module.exports = createJestRunner(require.resolve('./run'));
--- generator/fixtures/run.js
+++ generator/fixtures/run.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-// eslint-disable-next-line import/no-unresolved
 const { pass, fail } = require('create-jest-runner');
 
 module.exports = ({ testPath }) => {
