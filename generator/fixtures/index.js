0a5f246fe868a7c730434de6b7e0842de6345ed7
Mark Wheeler <mhwheeler@gmail.com>
2020-04-08 00:16:18 -0700

    fix: update dependencies (#26)
    BREAKING CHANGE: Drop node 8

--- generator/fixtures/index.js
+++ generator/fixtures/index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-unresolved
 const { createJestRunner } = require('create-jest-runner');
 
 module.exports = createJestRunner(require.resolve('./run'));
--- generator/fixtures/run.js
+++ generator/fixtures/run.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+// eslint-disable-next-line import/no-unresolved
 const { pass, fail } = require('create-jest-runner');
 
 module.exports = ({ testPath }) => {
