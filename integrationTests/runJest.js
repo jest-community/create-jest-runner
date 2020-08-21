e7add43a3cd383cc85cd8512f3a7d81ea67ed005
Simen Bekkhus <sbekkhus91@gmail.com>
2020-08-21 10:05:53 +0200

    normalize icons for windows

--- integrationTests/runJest.js
+++ integrationTests/runJest.js
@@ -14,7 +14,10 @@ const normalize = output =>
     .replace(new RegExp('.*at .*\\n', 'g'), 'mocked-stack-trace')
     .replace(/.*at .*\\n/g, 'mocked-stack-trace')
     .replace(/(mocked-stack-trace)+/, '      at mocked-stack-trace')
-    .replace(/\s+\n/g, '\n');
+    .replace(/\s+\n/g, '\n')
+    // https://github.com/facebook/jest/blob/a8addf8e22ef74b2ff3a139ece098604ec46b6e7/packages/jest-util/src/specialChars.ts#L11-L16
+    .replace(new RegExp('\u00D7', 'g'), '\u2715')
+    .replace(new RegExp('\u221A', 'g'), '\u2713');
 
 const runJest = (project, options = []) => {
   // eslint-disable-next-line
